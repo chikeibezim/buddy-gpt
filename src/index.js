515339c8c7457706977d9e5d91c13af44766a21a
Chike Ibezim <ibezimchike@gmail.com>
2023-02-13 19:26:35 +0100

    update to packages

--- src/index.js
+++ src/index.js
@@ -1,93 +1,68 @@
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+import dotenv from 'dotenv';
+dotenv.config();
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+import * as readline from 'node:readline/promises';
+import { Configuration, OpenAIApi } from 'openai';
+import { stdin as input, stdout as output } from 'node:process';
+import express from 'express';
 
-/**
- * Reads previously authorized credentials from the save file.
- *
- * @return {Promise<OAuth2Client|null>}
- */
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
+console.log(process.env.OPENAI_API_KEY)
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration);
+
+function generatePrompt(answer){
+  const capitalizedAnimal = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
+  return `Suggest three names for an animal that is a superhero.
+
+  Animal: Cat
+  Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+  Animal: Dog
+  Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+  Animal: ${capitalizedAnimal}
+  Names:`
 }
 
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
+async function check(){
+    if(!configuration.apiKey){
+      console.log("Invalid API key")
+      return;
+    }
+
+    const rl = readline.createInterface({ input, output });
+
+    const answer = await rl.question('Enter an Animal? ');
+
+    try{
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: generatePrompt(answer),
+        temperature: 0.6
+      });
+      console.log({ result: completion.data.choices[0].text})
+    }catch(err){
+      if(err.response){
+        console.error(err.response.status, err.response.data);
+      }else{
+        console.error(`Error with OpenAI API request: ${err.message}`);
+      }
+    }
 }
 
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
+check();
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  const res = await calendar.events.list({
-    calendarId: 'primary',
-    timeMin: new Date().toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
-  const events = res.data.items; 
-  if (!events || events.length === 0) {
-    console.log('No upcoming events found.');
-    return;
-  }
-  console.log('Upcoming 10 events:');
-  events.map((event, i) => {
-    const start = event.start.dateTime || event.start.date;
-    console.log(`${start} - ${event.summary}`);
-  });
-}
+/*
+const app = express()
+const port = 4000
 
-authorize().then(listEvents).catch(console.error);
+app.get('/', (req, res) => {
+  res.send('Hello Beanstalk!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+*/
